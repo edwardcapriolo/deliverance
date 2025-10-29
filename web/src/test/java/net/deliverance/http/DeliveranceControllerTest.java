cf043650100210e79d40bad50d57e384ce91250a
Edward Guy Capriolo <edlinuxguru@gmail.com>
2025-10-29 02:11:50 -0400

    bla

--- web/src/test/java/net/deliverance/http/DeliveranceControllerTest.java
+++ web/src/test/java/net/deliverance/http/DeliveranceControllerTest.java
@@ -1,9 +1,7 @@
 package net.deliverance.http;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.teknek.deliverance.generator.Generator;
 import io.teknek.deliverance.generator.Response;
-import io.teknek.deliverance.model.ChatCompletionRequestMessage;
 import io.teknek.deliverance.model.CreateChatCompletionRequest;
 import io.teknek.deliverance.model.CreateChatCompletionRequestStop;
 import org.junit.jupiter.api.Test;
@@ -11,7 +9,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import org.springframework.core.env.Environment;
@@ -22,12 +19,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import static org.apache.http.client.methods.RequestBuilder.post;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
@@ -40,14 +33,15 @@ public class DeliveranceControllerTest {
 
     @Autowired
     MockMvc mockMvc;
-    
+
     @Test
     public void whenUsingSpringBootTestArgs_thenCommandLineArgSet(@Autowired Environment env) throws Exception {
         Response r = new Response("yo", null,
                 null,0,0, 0, 0);
         when(generator.generate(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any()))
                 .thenReturn(r);
-        CreateChatCompletionRequest request = new CreateChatCompletionRequest().stop(new CreateChatCompletionRequestStop("stop"));
+        CreateChatCompletionRequest request = new CreateChatCompletionRequest()
+                .stop(null);
         io.teknek.deliverance.JSON j = new io.teknek.deliverance.JSON();
         String s = j.getMapper().writeValueAsString(request);
         mockMvc.perform(MockMvcRequestBuilders.post("/chat/completions")
