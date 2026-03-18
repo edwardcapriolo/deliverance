88a35678dac75184143ecac24671c3ff963bfc1e
Edward Guy Capriolo <edlinuxguru@gmail.com>
2026-03-18 17:44:15 +0000

    cleanup docker

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,7 @@
 
 
 cat << EOF > Dockerfile
-FROM ecapriolo/jdk-25:0.0.1 as base
+FROM ecapriolo/jdk-25:0.0.1 AS deliverance-base
 RUN apk add git
 RUN apk add maven
 
@@ -16,7 +16,7 @@ RUN git clone https://github.com/edwardcapriolo/deliverance.git
 
 RUN cd /build/deliverance
 WORKDIR /build/deliverance
-RUN --mount=type=cache,target=/root/.m2 cd /build/deliverance && mvn install -Dmaven.test.skip=true -Dgpg.skip=true
+RUN --mount=type=cache,target=/root/.m2 cd /build/deliverance && mvn install -Dmaven.test.skip.exec=true -Dgpg.skip=true
 
 #java -XX:+UnlockDiagnosticVMOptions \
 #-XX:CompilerDirectivesFile=../inlinerules.json \
@@ -24,16 +24,16 @@ RUN --mount=type=cache,target=/root/.m2 cd /build/deliverance && mvn install -Dm
 #--add-opens java.base/java.nio=ALL-UNNAMED --add-modules jdk.incubator.vector -Xmx3G \
 #-jar target/web-*-SNAPSHOT.jar
 
-FROM base as deliverance
+FROM base AS deliverance
 RUN mkdir /deliverance
 
-COPY --from=build /build/web/web-*.jar /deliverance
+COPY --from=deliverance-base /build/deliverance/web/web-*.jar /deliverance
 
 EOF
 
 DOCKER_BUILDKIT=1 docker build \
---target base \
--t base .
+--target deliverance-base \
+-t deliverance-base .
 
 DOCKER_BUILDKIT=1 docker build \
 --target deliverance \
