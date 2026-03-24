6cad15d3a638fd63c8838957c5ff92b8b0b39389
Edward Guy Capriolo <edlinuxguru@gmail.com>
2026-03-24 16:41:54 -0400

    Clean our serenity for the moment

--- core/src/test/java/io/teknek/deliverance/model/bert/BertTokenizerTest.java
+++ core/src/test/java/io/teknek/deliverance/model/bert/BertTokenizerTest.java
@@ -1,10 +1,11 @@
 package io.teknek.deliverance.model.bert;
 
 import io.teknek.deliverance.safetensors.fetch.ModelFetcher;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BertTokenizerTest {
     @Test
@@ -14,10 +15,9 @@ public class BertTokenizerTest {
         BertTokenizer bt = new BertTokenizer(f.toPath());
         //https://github.com/huggingface/transformers/blob/83fe012d58528a14ee0bb0146885f2d6fcb1ec3f/tests/tokenization/test_tokenization_utils.py#L129
         //from pretrained does not include leading space here
-        Assertions.assertEquals(" Force", bt.decode(2300));
-        Assertions.assertEquals(" [PAD]", bt.decode(0));
-        int last = bt.getModel().vocabLookup.size() - 1;
-        System.out.println(bt.getModel().vocabLookup);
+        assertEquals(" Force", bt.decode(2300));
+        assertEquals(" [PAD]", bt.decode(0));
+        //int last = bt.getModel().vocabLookup.size() - 1;
         //Assertions.assertEquals("##:", bt.decode(last));
     }
 }
