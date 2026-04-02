496b054438c1cae08bf44a4997c2f3dccc82c606
Edward Capriolo <edlinuxguru@gmail.com>
2026-04-01 21:50:28 -0400

    prepare for logprobs (#71)

--- tensor/src/main/java/io/teknek/deliverance/tensor/VectorTensorMathUtils.java
+++ tensor/src/main/java/io/teknek/deliverance/tensor/VectorTensorMathUtils.java
@@ -37,4 +37,23 @@ public class VectorTensorMathUtils {
         for (int i = 0; i < x.shape().last(); i++)
             x.set((float) (x.get(0, i) / magnitude), 0, i);
     }
+
+
+    public static void logSumExpTensor(AbstractTensor result, AbstractTensor input) {
+        float logsumexp = (float) logSumExp(input);
+        for (int i = 0; i < input.size(); i++) {
+            float v = input.get(0, i);
+            result.set(v - logsumexp, 0, i);
+        }
+    }
+
+    //https://nhigham.com/2021/01/05/what-is-the-log-sum-exp-function/
+    public static double logSumExp(AbstractTensor x){
+        float sum = 0.0f;
+        for (int i = 0; i < x.size(); i++) {
+            sum += (float) FastMath.exp(x.get(0, i));
+        }
+        float logsumexp = (float) FastMath.log(sum);
+        return logsumexp;
+    }
 }
---
+++ tensor/src/test/java/io/teknek/deliverance/tensor/VectorTensorMathUtilsTest.java
@@ -0,0 +1,49 @@
+package io.teknek.deliverance.tensor;
+
+import io.teknek.deliverance.tensor.impl.FloatBufferTensor;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class VectorTensorMathUtilsTest {
+
+    /*
+    From google ai (trust the answers with your life :)
+    import numpy as np
+    from scipy.special import logsumexp
+
+    logits = np.array([2.0, 1.0, 0.1])
+    # logsumexp provides numerical stability to avoid overflow
+    logprobs = logits - logsumexp(logits)
+
+    print(logprobs)
+    # Result: array([-0.417, -1.417, -2.317])
+    */
+
+    @Test
+    public void vectorToVector(){
+        int rows = 1;
+        int cols = 3;
+        AbstractTensor original = new FloatBufferTensor(rows, cols);
+        original.set(2.0f, 0, 0);
+        original.set(1.0f, 0, 1);
+        original.set(0.1f, 0, 2);
+        AbstractTensor result = new FloatBufferTensor(rows, cols);
+        VectorTensorMathUtils.logSumExpTensor(result, original);
+        String origin   = "[0][0]=  2.0000 [0][1]=  1.0000 [0][2]=  0.1000";
+        String expected = "[0][0]= -0.4170 [0][1]= -1.4170 [0][2]= -2.3170";
+        assertEquals(expected, TensorDisplayUtil.pretty2dDisplayAll(result).trim());
+    }
+
+    //https://nhigham.com/2021/01/05/what-is-the-log-sum-exp-function/
+    @Test
+    public void logSumExpTest(){
+        int rows =  1;
+        int cols = 3;
+        AbstractTensor original = new FloatBufferTensor(rows, cols);
+        original.set(1.0f, 0, 0);
+        original.set(2.0f, 0, 1);
+        original.set(3.0f, 0, 2);
+        assertEquals(3.4076058864593506f, VectorTensorMathUtils.logSumExp(original), 0.000001);
+    }
+}
