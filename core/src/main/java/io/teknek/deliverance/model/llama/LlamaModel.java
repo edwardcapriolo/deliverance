82f620ee28184e866c536191e84f502067238394
Edward Guy Capriolo <edlinuxguru@gmail.com>
2026-04-24 21:55:00 -0400

    Other models

--- core/src/main/java/io/teknek/deliverance/model/llama/LlamaModel.java
+++ core/src/main/java/io/teknek/deliverance/model/llama/LlamaModel.java
@@ -13,6 +13,7 @@ import io.teknek.deliverance.safetensors.WeightLoader;
 import io.teknek.deliverance.tensor.AbstractTensor;
 import io.teknek.deliverance.tensor.ArrayQueueTensorAllocator;
 import io.teknek.deliverance.tensor.KvBufferCacheSettings;
+import io.teknek.deliverance.tensor.TensorAllocator;
 import io.teknek.deliverance.tensor.operations.ConfigurableTensorProvider;
 import io.teknek.deliverance.tokenizer.Tokenizer;
 import io.teknek.deliverance.toolcallparser.ToolCallParser;
@@ -28,7 +29,7 @@ public class LlamaModel extends AbstractModel {
     public LlamaModel(InferenceType inferenceType, Config c, WeightLoader w, Tokenizer t, DType workingMemoryDType,
                       DType workingMemoryQType, Optional<DType> modelQType,
                       ConfigurableTensorProvider configurableTensorProvider, MetricRegistry metricRegistry,
-                      ArrayQueueTensorAllocator arrayQueueTensorAllocator, KvBufferCacheSettings kvBufferCacheSettings, TokenRenderer tokenRenderer,
+                      TensorAllocator arrayQueueTensorAllocator, KvBufferCacheSettings kvBufferCacheSettings, TokenRenderer tokenRenderer,
                       ToolCallParser toolCallParser, WrappedForkJoinPool pool) {
         super(inferenceType, c, w, t, workingMemoryDType, workingMemoryQType, modelQType, configurableTensorProvider,
                 metricRegistry, arrayQueueTensorAllocator, kvBufferCacheSettings, tokenRenderer, toolCallParser, pool);
--- core/src/main/java/io/teknek/deliverance/model/qwen2/Qwen2Model.java
+++ core/src/main/java/io/teknek/deliverance/model/qwen2/Qwen2Model.java
@@ -29,6 +29,7 @@ import io.teknek.deliverance.safetensors.Config;
 import io.teknek.deliverance.safetensors.WeightLoader;
 import io.teknek.deliverance.tensor.ArrayQueueTensorAllocator;
 import io.teknek.deliverance.tensor.KvBufferCacheSettings;
+import io.teknek.deliverance.tensor.TensorAllocator;
 import io.teknek.deliverance.tensor.operations.ConfigurableTensorProvider;
 import io.teknek.deliverance.tokenizer.Tokenizer;
 import io.teknek.deliverance.toolcallparser.ToolCallParser;
@@ -48,7 +49,7 @@ public class Qwen2Model extends LlamaModel {
             InferenceType inferenceType, Config c, WeightLoader w, Tokenizer t, DType workingMemoryDType,
             DType workingMemoryQType, Optional<DType> modelQType,
             ConfigurableTensorProvider configurableTensorProvider, MetricRegistry metricRegistry,
-            ArrayQueueTensorAllocator arrayQueueTensorAllocator, KvBufferCacheSettings kvBufferCacheSettings, TokenRenderer tokenRenderer,
+            TensorAllocator arrayQueueTensorAllocator, KvBufferCacheSettings kvBufferCacheSettings, TokenRenderer tokenRenderer,
             ToolCallParser toolCallParser, WrappedForkJoinPool pool
     ) {
         super(inferenceType, c, w, t, workingMemoryDType, workingMemoryQType, modelQType, configurableTensorProvider, metricRegistry,
