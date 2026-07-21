d14d36b8be1a0cbd33a7ca7e219641ecf546a712
Edward Guy Capriolo <edlinuxguru@gmail.com>
2026-07-21 04:23:11 +0000

    Add Spring generated Deliverance client
    Signed-off-by: Edward Guy Capriolo <edlinuxguru@gmail.com>

--- web/src/test/java/io/teknek/deliverance/web/ClientE2ETest.java
+++ web/src/test/java/io/teknek/deliverance/web/ClientE2ETest.java
@@ -1,12 +1,15 @@
 package io.teknek.deliverance.web;
 
+import java.time.Duration;
+
 import io.teknek.deliverance.client.api.ModelsApi;
 import io.teknek.deliverance.client.model.ListModelsResponse;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
@@ -19,9 +22,26 @@ public class ClientE2ETest {
     @LocalServerPort
     int port;
 
-    @Test
-    void shouldListModelsViaClient() throws Exception {
+    @ParameterizedTest
+    @ValueSource(strings = { "retrofit", "spring" })
+    void shouldListModelsViaClient(String client) throws Exception {
         String baseUrl = "http://localhost:" + port + "/";
+
+        if ("retrofit".equals(client)) {
+            ListModelsResponse response = listModelsWithRetrofitClient(baseUrl);
+
+            assertThat(response).isNotNull();
+            assertThat(response.getData()).isNotNull();
+        }
+        else {
+            io.teknek.deliverance.client.spring.model.ListModelsResponse response = listModelsWithSpringClient(baseUrl);
+
+            assertThat(response).isNotNull();
+            assertThat(response.getData()).isNotNull();
+        }
+    }
+
+    private ListModelsResponse listModelsWithRetrofitClient(String baseUrl) throws Exception {
 
         String auth = java.util.Base64.getEncoder().encodeToString("1:2".getBytes());
 
@@ -44,9 +64,19 @@ public class ClientE2ETest {
 
         ModelsApi api = retrofit.create(ModelsApi.class);
 
-        ListModelsResponse response = api.listModels().execute().body();
+        return api.listModels().execute().body();
+    }
 
-        assertThat(response).isNotNull();
-        assertThat(response.getData()).isNotNull();
+    private io.teknek.deliverance.client.spring.model.ListModelsResponse listModelsWithSpringClient(String baseUrl) {
+        String auth = java.util.Base64.getEncoder().encodeToString("1:2".getBytes());
+        io.teknek.deliverance.client.spring.core.ApiClient apiClient = new io.teknek.deliverance.client.spring.core.ApiClient()
+                .setBasePath(baseUrl)
+                .addDefaultHeader("Authorization", "Basic " + auth)
+                .addDefaultHeader("Content-Type", "application/json")
+                .addDefaultHeader("Accept", "application/json");
+        io.teknek.deliverance.client.spring.api.ModelsApi api = new io.teknek.deliverance.client.spring.api.ModelsApi(
+                apiClient);
+
+        return api.listModels().block(Duration.ofSeconds(5));
     }
 }
